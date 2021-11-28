67158af21a1bf24fa09d032fca480ede987f42b5
Michał Szyba <m.szyba@gmail.com>
2021-11-28 12:29:22 +0100

    Initial commit

---
+++ src/main/java/eu/michalszyba/adrwaybill/AdrWaybillApplication.java
@@ -0,0 +1,13 @@
+package eu.michalszyba.adrwaybill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdrWaybillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdrWaybillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/michalszyba/adrwaybill/AdrWaybillApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.michalszyba.adrwaybill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdrWaybillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
