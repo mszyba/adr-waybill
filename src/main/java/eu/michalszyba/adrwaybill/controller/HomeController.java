32fcdbee22d0fea3c0abbd3780dff62cea744fb0
Michał Szyba <m.szyba@gmail.com>
2022-04-07 18:03:58 +0200

    added homepage

--- src/main/java/eu/michalszyba/adrwaybill/controller/HomeController.java
+++ src/main/java/eu/michalszyba/adrwaybill/controller/HomeController.java
@@ -1,18 +1,30 @@
 package eu.michalszyba.adrwaybill.controller;
 
+import eu.michalszyba.adrwaybill.model.Customer;
+import eu.michalszyba.adrwaybill.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final CustomerService customerService;
+
+    public HomeController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @ModelAttribute("customers")
+    public List<Customer> populateCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+
     @GetMapping(path = {"/", "/home"})
     public String home() {
         return "home";
-    }
-
-    @GetMapping(path = { "/index"})
-    public String index() {
-        return "index";
     }
 }
